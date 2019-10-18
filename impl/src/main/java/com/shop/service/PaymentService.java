67ee783ba1cb305656a839627248c1d8b6431d30
igor <sawqe1@yandex.ru>
2019-10-18 18:06:39 +0300

    add OrderServiceSender into PaymentService

--- impl/src/main/java/com/shop/service/PaymentService.java
+++ impl/src/main/java/com/shop/service/PaymentService.java
@@ -3,6 +3,8 @@ package com.shop.service;
 import com.shop.api.swagger.models.OrderDto;
 import com.shop.api.swagger.models.PaymentDto;
 import com.shop.api.swagger.models.Status;
+import com.shop.client.OrderServiceClient;
+import com.shop.client.OrderServiceSender;
 import com.shop.model.Payment;
 import com.shop.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +20,12 @@ import java.util.stream.StreamSupport;
 @Slf4j
 public class PaymentService {
     private PaymentRepository repository;
+    private OrderServiceSender orderSender;
 
     @Autowired
-    public PaymentService(PaymentRepository repository) {
+    public PaymentService(PaymentRepository repository, OrderServiceSender orderServiceSender) {
         this.repository = repository;
+        this.orderSender = orderServiceSender;
     }
 
     public PaymentDto pay(OrderDto order) {
@@ -29,6 +33,8 @@ public class PaymentService {
         payment.setCustomerId(order.getCustomerId());
         payment.setOrderId(order.getId());
         payment.setStatus(Status.OK);
+        repository.save(payment);
+        this.orderSender.sendMessage(order.getId(), payment.getId());
         return convertToDTO(payment);
     }
 
